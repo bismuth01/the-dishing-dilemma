0b8b606accdd4af135ec3c953c850f1617a2383f
EliteGamerSiddhu <siddharthaswarnkar@gmail.com>
2025-02-21 03:08:03 +0530

    completed without good UI

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let choicesHistory = [];
+
+function saveChoice(option) {
+  choicesHistory.push(option);
+}
+
+function wasChoiceMade(option) {
+  return choicesHistory.includes(option);
+}
+
 function startGame() {
   changeBackground("./images/washing-dishes.jpg");
   showText("You're washing dishes when the doorbell rings...");
@@ -13,18 +23,15 @@ function startGame() {
 }
 
 function changeBackground(imageUrl) {
-  document.getElementById("game-container").style.backgroundImage =
-    `url(${imageUrl})`;
+  setTimeout(() => {
+    document.getElementById("game-container").style.backgroundImage =
+      `url(${imageUrl})`;
+  }, 1000);
 }
 
 function showText(text) {
   let textBox = document.getElementById("text-box");
   textBox.innerText = text;
-  textBox.classList.add("show-text");
-
-  setTimeout(() => {
-    textBox.classList.remove("show-text");
-  }, 5000);
 }
 
 function showChoices(choices) {
@@ -37,22 +44,67 @@ function showChoices(choices) {
 }
 
 function choose(option) {
+  saveChoice(option);
   if (option === "answerDoor") {
+    if (wasChoiceMade("closeTap") && wasChoiceMade("askWho")) {
+      changeBackground("./images/annoyed-neighbour.jpg");
+      showText("Neighbour is there. She seems annoyed...");
+    } else if (wasChoiceMade("finishDishes")) {
+      changeBackground("./images/package.jpeg");
+      showText("It seems the person left...");
+    } else {
+      changeBackground("./images/happy-neighbour.jpeg");
+      showText("You open the door...");
+    }
+    showChoices([{ option: "takePackage", text: "Take Package" }]);
+  } else if (option === "closeTap") {
+    changeBackground("./images/closed-tap.jpg");
+    showText("You close the tap...");
     changeBackground("./images/hallway-door.jpg");
-    showText("You open the door...");
-    unlockEnding("Flooded Home");
-  } else {
+    showChoices([
+      { option: "answerDoor", text: "Answer the Door" },
+      { option: "askWho", text: "Ask who's at the door" },
+    ]);
+  } else if (option === "finishDishes") {
     changeBackground("./images/finish-dishes.jpg");
-    showText("You finish the dishes first...");
-    unlockEnding("Dry Home");
+    showText("You finish the dishes...");
+    changeBackground("./images/hallway-door.jpg");
+    showChoices([
+      { option: "answerDoor", text: "Answer the Door" },
+      { option: "askWho", text: "Ask who's at the door" },
+    ]);
+  } else if (option === "takePackage") {
+    if (!wasChoiceMade("finishDishes")) {
+      changeBackground("./images/sign-paper.jpg");
+      showText("You sign the papers and take the package...");
+    }
+    if (wasChoiceMade("closeTap") || wasChoiceMade("finishDishes")) {
+      changeBackground("./images/package-inside.jpeg");
+      showText("You took the package inside...");
+      if (wasChoiceMade("closeTap")) {
+        unlockEnding("Let's see the package");
+      } else if (wasChoiceMade("finishDishes")) {
+        unlockEnding("A lonely package");
+      }
+    } else {
+      changeBackground("./images/flooded.jpeg");
+      showText(
+        "You forgot to turn off the tap. Now you have a package and a flooded apartment",
+      );
+      unlockEnding("Flooded with a package");
+    }
+  } else if (option === "askWho") {
+    if (wasChoiceMade("closeTap")) {
+      showText("It's me, Karen!!");
+    } else if (wasChoiceMade("finishDishes")) {
+      showText("Silence....");
+    }
+    showChoices([{ option: "answerDoor", text: "Answer the Door" }]);
   }
-
-  setTimeout(() => {
-    location.reload();
-  }, 4000);
 }
 
 function unlockEnding(endingName) {
+  document.getElementById("choice-container").style.display = `none`;
   let endings = JSON.parse(localStorage.getItem("endings")) || [];
 
   if (!endings.includes(endingName)) {
